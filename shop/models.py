5eb4ea69b21a08ae5c46eb245b11e10bf6c00e37
Askarjon Abdullayev <askarjon.abdullayev@gmail.com>
2021-02-17 11:57:33 +0500

    start

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
+from django.shortcuts import reverse
+from django.template.defaultfilters import slugify
+from ckeditor.fields import RichTextEditor
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=None, decimal_places=None)
+    seller = models.ForeignKey(User)
+    brand_choices = [
+       ('apple','apple'),
+        ('samsung','samsung'),
+    ]
+    brand = models.CharField(max_length=30,null=True, blank=True,choices=brand_choices)
+    short_description = models.TextField(blank=True)
+
+    color_choices = [
+        ('black', 'black'),
+    ]
+    colors = models.CharField(max_length=30, null=True,
+                              blank=True, choices=color_choices)
+    
+    photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    in_stock = models.BooleanField(default=True)
+    product_date = models.DateTimeField(default=datetime.now, blank=True)
+    slug = models.SlugField()
+
+    description = RichTextEditor(blank=True, null=True)
+
+    def get_absolute_url(self):
+        return reverse("product_detail", kwargs={"slug": self.slug})
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.title)
+
+        super(Product, self).save(*args, **kwargs)
+    
+
